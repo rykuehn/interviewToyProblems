a29e81d8eab0c111d5046c72375b4de1126446b5
Robin Kuehn <kuehn.robin@gmail.com>
2017-01-21 10:51:18 -0800

    Binary Search tree -- need to work on balancing

--- balancedBinaryTree.js
+++ balancedBinaryTree.js
@@ -22,11 +22,79 @@ BinaryTreeNode.prototype.insert = function(value){
   }
 }
 
+BinaryTreeNode.prototype.contains = function(value) {
+
+  if(value === this.value) {
+    return true;
+  }
+
+  var traverse = function(node, target) {
+    let flag = false;
+
+    if(node.value === target) {
+      flag = true;
+    }
+    if(target > node.value && node.right !== null) {
+      flag = traverse(node.right, target);
+    }
+
+    if(target < node.value && node.left !== null) {
+      flag =traverse(node.left, target);
+    }
+
+    return flag;
+
+  }
+
+  return traverse(this, value)
+}
+
+BinaryTreeNode.prototype.balanced = function() {
+  var depths = [];
+
+  var nodes = [];
+  nodes.push([this, 0]);
+
+  while(nodes.length) {
+    var nodePair = nodes.pop();
+    var node = nodePair[0];
+    var depth = nodePair[1];
+
+    if(!node.left && !node.right) {
+      if(depths.indexOf(depth) < 0) {
+        depths.push(depth);
+      }
+
+      if((depths.length > 2) || (depths.length === 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+        return false;
+      }
+
+    } else{
+      if(node.left) {
+        nodes.push([node.left, depth + 1]);
+      }
+
+      if(node.right) {
+        nodes.push([node.right, depth + 1]);
+      }
+    }
+    
+  }
+
+  return true;
+}
+
+
+
+
+
 var mine = new BinaryTreeNode(5)
 //console.log(mine.insert(4))
 mine.insert(4)
-mine.insert(8)
-mine.insert(10)
-console.log(mine);
+mine.insert(3)
+mine.insert(1)
+console.log(mine.balanced())
+
+//console.log(mine);
 
 
