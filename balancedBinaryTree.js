b1e31bd747c777fa11ed284124034610bc1f364f
Robin Kuehn <kuehn.robin@gmail.com>
2017-01-22 21:58:24 -0800

    refractor

--- balancedBinaryTree.js
+++ balancedBinaryTree.js
@@ -85,30 +85,12 @@ BinaryTreeNode.prototype.balanced = function() {
 }
 
 
-function bstChecker(tree, upperBound, lowerBound) {
-  //console.log(upperBound, lowerBound)
-  upperBound = (typeof upperBound === 'undefined') ? Infinity : upperBound;
-  lowerBound = (typeof lowerBound === 'undefined') ? -Infinity : lowerBound;
-
-  if(!tree) { return true }
-  
-  if(tree.value < lowerBound || tree.value > upperBound) {
-    return false;
-  } 
-
-  return bstChecker(tree.left, tree.value, lowerBound) 
-  && bstChecker(tree.right, lowerBound, tree.value);
-}
-
-
-
-
-var mine = new BinaryTreeNode(5)
-//console.log(mine.insert(4))
-mine.insert(4)
-mine.insert(3)
-mine.insert(1)
-console.log(bstChecker(mine))
+// var mine = new BinaryTreeNode(5)
+// //console.log(mine.insert(4))
+// mine.insert(4)
+// mine.insert(3)
+// mine.insert(1)
+// console.log(bstChecker(mine))
 
 //console.log(mine);
 
--- bstChecker.js
+++ bstChecker.js
@@ -1,3 +1,15 @@
-function bstChecker(tree) {
+
+function bstChecker(tree, upperBound, lowerBound) {
+  //console.log(upperBound, lowerBound)
+  upperBound = (typeof upperBound === 'undefined') ? Infinity : upperBound;
+  lowerBound = (typeof lowerBound === 'undefined') ? -Infinity : lowerBound;
+
+  if(!tree) { return true }
   
+  if(tree.value < lowerBound || tree.value > upperBound) {
+    return false;
+  } 
+
+  return bstChecker(tree.left, tree.value, lowerBound) 
+  && bstChecker(tree.right, lowerBound, tree.value);
 }
